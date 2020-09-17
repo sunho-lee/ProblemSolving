2617ad2995b289ee5130e4a0f5a92e30466638aa
sunho-lee <sh112123@naver.com>
2020-09-17 16:22:43 +0900

    Update 11053_가장긴증가하는부분수열.cpp

--- BOJ/silver/11053_가장긴증가하는부분수열.cpp
+++ BOJ/silver/11053_가장긴증가하는부분수열.cpp
@@ -6,19 +6,19 @@ using namespace std;
 
 int D[1001];
 int A[1001];
-int ans = 0;
+int ans;
 int function(int n){
 
-	for(int i = 1; i <= n; i++){
-		if(A[i] > A[i-1]){
-			D[i] = D[i-1] + 1;
-			cout<<"i = " << i <<" A["<< i<<"] = " << A[i] << " A[" << i - 1<< "] = " << A[i - 1] << " D["<< i <<"] = " << D[i] <<  '\n';
+	for(int i = 2; i <= n; i++){
+		for(int j = i - 1 ; j >= 1; j--){
+			if(A[i] > A[j]){
+				D[i] = max(D[i], D[j] + 1);
+			}
 		}
 	}
-	
-	for(int i =0; i <= n; i++){
+
+	for(int i = 1; i <= n; i++){
 		ans = max(ans, D[i]);
-	//	cout << "D["<< i <<"] = " << D[i] << " D["<<i - 1<<"] = " << D[i - 1] << " ans = " << ans <<'\n';
 	}
 	
 	return ans;
@@ -26,18 +26,18 @@ int function(int n){
 
 int main(){
 	int n;
-	cin >> n;
 
 	ios_base:: sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 
-	memset(D, 0, sizeof(D));
-	
+	cin >> n;
+
 	for(int i = 1; i <= n; i++){
 		cin >> A[i];
 	}
-	
+	D[1] = 1;
+
 	cout << function(n);
 
     return 0;
